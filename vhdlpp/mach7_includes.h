e3ad5e2181ff33b7ceddbb368f53e5341b77e24a
forflo <mayerflorian@me.com>
2016-07-03 22:02:18 +0200

    Added mach7_includes.h which simplifies usage of mach7 a little

---
+++ vhdlpp/mach7_includes.h
@@ -0,0 +1,11 @@
+#ifndef IVL_MACH7_SETUP
+#define IVL_MACH7_SETUP
+
+// mach7 setup
+#include <type_switchN-patterns.hpp> // Support for N-ary Match statement on patterns
+#include <address.hpp>      // Address and dereference combinators
+#include <bindings.hpp>     // Mach7 support for bindings on arbitrary UDT
+#include <constructor.hpp>  // Support for constructor patterns
+#include <equivalence.hpp>  // Equivalence combinator +
+
+#endif // IVL_MACH7_SETUP
