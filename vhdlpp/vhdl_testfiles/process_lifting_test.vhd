289ec6f4d8095ae3a2ffd29a7dd044e1314b97bd
forflo <mayerflorian@me.com>
2016-07-30 17:32:39 +0200

    Added test source for process lifting test

---
+++ vhdlpp/vhdl_testfiles/process_lifting_test.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity ent is
+   generic(n : natural := 2);
+   port(A     : in  std_logic_vector(n - 1 downto 0);
+        B     : in  std_logic_vector(n - 1 downto 0);
+        carry : out std_logic;
+        sum   : out std_logic_vector(n - 1 downto 0));
+end ent;
+
+architecture beh of ent is
+   signal result : std_logic_vector(n downto 0);
+begin
+   result <= ('0' & A) + ('0' & B);
+   sum    <= result(n - 1 downto 0);
+   carry  <= result;
+end beh;
