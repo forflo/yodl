8f5119ff942d5804cf3f4d2152e4f7b399b630fa
forflo <mayerflorian@me.com>
2016-08-23 19:27:49 +0200

    Removed unwanted files

--- vhdlpp/vhdlpp_config.h
+++ vhdlpp/vhdlpp_config.h
@@ -28,11 +28,11 @@
 #  endif
 #endif
 
-# define HAVE_GETOPT_H 1
+/* # undef HAVE_GETOPT_H */
 # define HAVE_INTTYPES_H 1
 /* # undef HAVE_LIBIBERTY_H */
 /* # undef HAVE_FCHMOD */
-# define HAVE_SYS_WAIT_H 1
+/* # undef HAVE_SYS_WAIT_H */
 
 #ifdef HAVE_INTTYPES_H
 # include  <inttypes.h>
